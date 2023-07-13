cebe5bc91d14970ae19586413c65b8dfdeb412f5
Massimiliano-D <massimiliano.dorsi96@gmail.com>
2023-07-13 23:37:58 +0200

    estrazione riuscita

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const TAB = document.getElementById("TOMBOLONE");
 for (let i = 1; i <= 76; i++) {
   const cella = document.createElement("div");
   cella.classList.add("cella");
+  cella.classList.add("all");
   cella.innerText = i;
   TAB.appendChild(cella);
 }
-for (let j = 1; j <= 76; i++) {
-  const numero = document.createElement("div");
-  numero.classList.add("numero");
-  cella.innerText = i;
-  TAB.appendChild(numero);
-}
+
+let randomIndex = 0;
+const estraibot = document.getElementById("estraibot");
+estraibot.onclick = function () {
+  const arrynum = document.getElementsByClassName("all");
+  randomIndex = Math.floor(Math.random() * 76 + 1);
+  // console.log(randomIndex);
+  // console.log(arrynum);
+  for (i = 0; i < arrynum.length; i++) {
+    if (randomIndex === parseInt(arrynum[i].innerText)) {
+      arrynum[i].classList.add("uscito");
+    }
+  }
+};
